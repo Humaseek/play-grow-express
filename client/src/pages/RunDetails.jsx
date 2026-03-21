f6e63db0d0ecadb12bd057636ad81742d2fff3ae
Leonardo <tamerjbara522@gmail.com>
2026-03-21 12:30:28 +0200

    update

--- client/src/pages/RunDetails.jsx
+++ client/src/pages/RunDetails.jsx
@@ -3215,7 +3215,6 @@ export default function RunDetails() {
                       <th>تاريخ الشراء</th>
                       <th>عدد الجلسات</th>
                       <th>السعر (₪)</th>
-                      <th>الحالة</th>
                       <th style={{ textAlign: "center" }}>إجراءات</th>
                     </tr>
                   </thead>
