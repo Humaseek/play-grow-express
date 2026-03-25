02f1781031a803f73ab035513262a8679c2fb38d
Leonardo <tamerjbara522@gmail.com>
2026-03-26 00:30:18 +0200

    update

--- client/src/pages/ChildDetails.jsx
+++ client/src/pages/ChildDetails.jsx
@@ -194,7 +194,7 @@ const PROFILE_STYLES = `
   }
 
   .modal-form-scroll-container {
-    max-height: calc(85vh - 250px) !important; 
+    max-height: calc(85vh - 500px) !important; 
     padding: 0 5px;
   }
 
