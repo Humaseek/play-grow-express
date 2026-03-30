6cf1ebebc3ddd0752c8d1f9836a5cefd8e695946
Leonardo <tamerjbara522@gmail.com>
2026-03-30 20:34:41 +0300

    update

--- client/src/pages/Today.jsx
+++ client/src/pages/Today.jsx
@@ -435,7 +435,7 @@ const DualBarChart = ({ incomeData, expenseData, labels }) => {
           <div style={{
             position: "absolute",
             top: 20,
-            left: "50%",
+            left: `${((hoveredIdx + 0.5) / labels.length) * 100}%`,
             transform: "translateX(-50%)",
             zIndex: 50,
             background: "#0f172a",
